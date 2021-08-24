b9394f5a84899570b7cdf134d4f6bbe9fdf00fec
Manuel Robert <manuelrobz@outlook.com>
2021-08-24 22:12:40 +0530

    aws secret manager set

--- main_2_enc-dec.py
+++ main_2_enc-dec.py
@@ -35,4 +35,4 @@ def get_data():
     return {'msg':'ok'}, 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=3000)
+    app.run(debug=True, host='127.0.0.1', port=3000)
